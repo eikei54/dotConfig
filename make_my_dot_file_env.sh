687c5776d79ca8eb0846984d30b7cd6df6d0080c
eikei54 <kami1030@yahoo.co.jp>
2018-10-28 21:58:50 +0900

    added bash functions

--- make_my_dot_file_env.sh
+++ make_my_dot_file_env.sh
@@ -8,6 +8,7 @@ rm .minttyrc
 rm .gitconfig
 rm .vimrc
 rm .bash_profile
+rm .bash_functions
 rm .gitignore
 rm .tigrc
 rm .tmux.conf
@@ -17,6 +18,7 @@ ln -s ./dotConfig/_minttyrc .minttyrc
 ln -s ./dotConfig/_gitconfig .gitconfig
 ln -s ./dotConfig/_vimrc .vimrc
 ln -s ./dotConfig/_bash_profile .bash_profile
+ln -s ./dotConfig/_bash_functions .bash_functions
 ln -s ./dotConfig/_gitignore .gitignore
 ln -s ./dotConfig/_tigrc .tigrc
 ln -s ./dotConfig/_tmux.conf .tmux.conf
