da2a33afe9e4e3bc53cf5d5139256c6fee04502f
eikei54 <kami1030@yahoo.co.jp>
2019-04-23 13:22:16 +0900

    updated make_my_dot_file_env

--- make_my_dot_file_env.sh
+++ make_my_dot_file_env.sh
@@ -7,19 +7,23 @@
 rm .minttyrc
 rm .gitconfig
 rm .vimrc
+rm .gvimrc
 rm .bash_profile
 rm .bash_functions
 rm .gitignore
 rm .tigrc
 rm .tmux.conf
+rm .agignore
 
 # make symbolic link
 ln -s ./dotConfig/_minttyrc .minttyrc
 ln -s ./dotConfig/_gitconfig .gitconfig
 ln -s ./dotConfig/_vimrc .vimrc
+ln -s ./dotConfig/_gvimrc .gvimrc
 ln -s ./dotConfig/_bash_profile .bash_profile
 ln -s ./dotConfig/_bash_functions .bash_functions
 ln -s ./dotConfig/_gitignore .gitignore
 ln -s ./dotConfig/_tigrc .tigrc
 ln -s ./dotConfig/_tmux.conf .tmux.conf
+ln -s ./dotConfig/_agignore .agignore
 
